f2e051eb2c22b37727adb743ab4e2299536f29df
aad3rinto <aad3rinto@gmail.com>
2025-08-04 16:22:45 +0100

    solved the leap year finder chanllenge

--- Day 10/Docstrings/task.py
+++ Day 10/Docstrings/task.py
@@ -1,15 +1,16 @@
-def format_name(f_name, l_name):
-    formated_f_name = f_name.title()
-    formated_l_name = l_name.title()
-    return f"{formated_f_name} {formated_l_name}"
+def is_leap_year(year):
+    """Check if a year is a leap year."""
+    # Write your code here.
+    # Don't change the function name.
+    if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
+        return "Leap Year"
+    elif year % 4 != 0:
+        return "Not a leap year"
+    elif year % 4 == 0 and year % 100 == 0 and year % 400 != 0:
+        return "Not a leap year"
+    else:
+        return "leap year"
 
 
-formatted_name = format_name("AnGeLa", "YU")
 
-length = len(formatted_name)
-
-
-
-def my_function(num):
-    """Multiplies a number by itself."""
-    return num * num
+print(is_leap_year(2000))
