ba0aa53d9c3238179daa3ecc6fd839bbf903fea2
aad3rinto <aad3rinto@gmail.com>
2025-08-14 15:23:44 +0100

    stating off day 13
    Signed-off-by: aad3rinto <aad3rinto@gmail.com>

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -7,6 +7,14 @@ def my_function():
 my_function()
 
 # Describe the Problem - Write your answers as comments:
+# The print statement is not working, Should be printing "You got it"
+
 # 1. What is the for loop doing?
+# the for loop is meant to loop from 1 to 20
+
 # 2. When is the function meant to print "You got it"?
+# Meant to print when the iterative is equal to 20
+
 # 3. What are your assumptions about the value of i?
+# assumption is that i will reach 20 on it's last run
+
